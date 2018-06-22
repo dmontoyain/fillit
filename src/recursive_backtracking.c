7bb54c398b554e90ad02c86c6612cecb3fc18fd7
dmontoyain <dmontoyainestroza@gmail.com>
2018-06-21 22:12:55 -0700

    more organizing; Modified Makefile

---
+++ src/recursive_backtracking.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   recursive_backtracking.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontoya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/18 22:52:55 by dmontoya          #+#    #+#             */
+/*   Updated: 2017/10/21 17:16:17 by dmontoya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	empty_grid(char *grid, int y)
+{
+	char	a;
+	int		i;
+
+	i = 0;
+	if (y == 0)
+		y++;
+	a = (y - 1) + 65;
+	while (grid[i] != '\0')
+	{
+		if (grid[i] == a)
+			grid[i] = '.';
+		i++;
+	}
+}
+
+int		fillit(char *grid, int *tetconf, int i, int y)
+{
+	int t;
+
+	t = 0;
+	while (t < 4)
+	{
+		if (t == 0)
+			grid[i] = y + 65;
+		else
+			grid[i + tetconf[t]] = y + 65;
+		t++;
+	}
+	return (1);
+}
+
+int		finished_recursion(int y, char *grid, int tetcount)
+{
+	if (tetcount == y + 1)
+	{
+		if (tetcount - 1 == 0)
+			ft_putstr(grid);
+		return (1);
+	}
+	return (-1);
+}
+
+int		recursive_backtrack(char *grid, int **tetconf, int y, int tetcount)
+{
+	int	x;
+
+	x = -1;
+	while (grid[++x] != '\0')
+	{
+		if (placement(grid, tetconf[y], ft_determinesize(grid), x) != -1)
+		{
+			fillit(grid, tetconf[y], x, y);
+			if (y + 1 != tetcount)
+				tetcount = recursive_backtrack(grid, tetconf, y + 1, tetcount);
+			if (finished_recursion(y, grid, tetcount) == 1)
+				return (--tetcount);
+		}
+		else if (grid[x + 1] == '\0' && y == 0)
+		{
+			x = -1;
+			grid = squaresize(ft_determinesize(grid) + 1);
+			tetconf = ft_figadjust(tetconf, ft_determinesize(grid), tetcount);
+		}
+	}
+	empty_grid(grid, y);
+	return (tetcount);
+}
+
+void	findbesttetris(int **tetconf, int tetcount)
+{
+	int		size;
+	char	*grid;
+	int		y;
+
+	y = 0;
+	if (tetcount == 1 && tetconf[0][1] == 1 && tetconf[0][2] == 5
+		&& tetconf[0][3] == 6)
+		tetcount = ft_onetet(tetconf, tetcount);
+	size = 3;
+	if (tetcount <= 2 && tetcount > 0 && size == 3)
+	{
+		tetconf = ft_figadjust(tetconf, size, tetcount);
+		grid = squaresize(size);
+		tetcount = recursive_backtrack(grid, tetconf, y, tetcount);
+		if (tetcount > 0)
+			tetconf = ft_figadjust(tetconf, size, tetcount);
+	}
+	if (tetcount != 0)
+	{
+		size = 4;
+		grid = squaresize(size);
+		recursive_backtrack(grid, tetconf, y, tetcount);
+	}
+}
